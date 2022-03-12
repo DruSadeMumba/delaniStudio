a723b841dd18ac5e6d56956353a68ee7cdbba9c5
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-12 13:13:14 +0300

    Add jquery functions for the contact us form

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 /* BUSINESS LOGIC */
+function validate(){
+    var username = document.getElementById("user-name");
+    var email = document.getElementById("user-email");
+    var text = document.getElementById("user-text");
+
+    if  (username.value == "" || email.value == "" || text.value == "") {
+        alert ("Please input required fields.");
+        return false;
+    }else{
+        alert ("We have received your message. Thank you for reaching out to us.");
+        return true;
+    }
+    event.preventDefault();
+}
 
 
 
@@ -23,7 +37,7 @@ $(document).ready(function(){
     });
 
     $(".clickable3").click(function(){
-        $("#project-management").toggle();
+        $("#product-management").toggle();
         $("#hidden-pm").toggle();
     });
 });
