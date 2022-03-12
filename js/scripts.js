a4c60ce1de7f0a66cafb5625183a2c7718a48fee
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-12 16:44:37 +0300

    Add media queries to portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 /* BUSINESS LOGIC */
 function validate(){
-    var username = document.getElementById("user-name");
-    var email = document.getElementById("user-email");
-    var text = document.getElementById("user-text");
+  var username = document.getElementById("user-name");
+  var email = document.getElementById("user-email");
+  var text = document.getElementById("user-text");
 
-    if  (username.value == "" || email.value == "" || text.value == "") {
-        alert ("Please input required fields.");
-        return false;
-    }else{
-        alert ("We have received your message. Thank you for reaching out to us.");
-        return true;
-    }
-    event.preventDefault();
+  if  (username.value == "" || email.value == "" || text.value == "") {
+    alert ("Please input required fields.");
+    return false;
+  }else{
+    alert ("We have received your message. Thank you for reaching out to us.");
+    return true;
+  }
+  event.preventDefault();
 }
-
-
-
-
-
-
 
 
 
@@ -26,18 +20,23 @@ function validate(){
 
 /* UI LOGIC */
 $(document).ready(function(){
-    $(".clickable1").click(function(){
-        $("#design").toggle();
-        $("#hidden-design").toggle();
-    });
+  $(".clickable1").click(function(){
+    $("#design").toggle();
+    $("#hidden-design").toggle();
+	});
 
-    $(".clickable2").click(function(){
-        $("#development").toggle();
-        $("#hidden-development").toggle();
-    });
+  $(".clickable2").click(function(){
+    $("#development").toggle();
+    $("#hidden-development").toggle();
+  });
 
-    $(".clickable3").click(function(){
-        $("#product-management").toggle();
-        $("#hidden-pm").toggle();
-    });
+  $(".clickable3").click(function(){
+    $("#product-management").toggle();
+    $("#hidden-pm").toggle();
+  });
+
+
+  $("#").hover(function (){
+    $()
+  });
 });
