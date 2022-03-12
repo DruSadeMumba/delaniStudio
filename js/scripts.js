5ee922ca4bc79141bb144facdf54f06c0146fd5e
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-12 10:24:53 +0300

    Make the what-we-do icons individually interactive.

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,18 @@
 
 /* UI LOGIC */
 $(document).ready(function(){
+    $(".clickable1").click(function(){
+        $("#design").toggle();
+        $("#hidden-design").toggle();
+    });
 
+    $(".clickable2").click(function(){
+        $("#development").toggle();
+        $("#hidden-development").toggle();
+    });
+
+    $(".clickable3").click(function(){
+        $("#project-management").toggle();
+        $("#hidden-pm").toggle();
+    });
 });
